6087fa3ff62375ac41ee2da642a2daac82db8ba6
Shayirbek Abdymomunov <legionera803@gmail.com>
2023-07-13 17:05:21 +0600

    backspace btn and enter btn are created

--- src/containers/DoorLock/DoorLock.tsx
+++ src/containers/DoorLock/DoorLock.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BUTTONS} from "../../constants";
 import {useDispatch, useSelector} from "react-redux";
-import {btnClick, onValueChange} from "./doorLockSlice";
+import {backSpace, btnClick, enter, onValueChange} from "./doorLockSlice";
 import {typeInitState} from "../../types";
 
 const DoorLock = () => {
@@ -10,8 +10,12 @@ const DoorLock = () => {
     const dispatch = useDispatch();
 
     return (
-        <form>
-            <input type="password"
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            dispatch(enter());
+        }}>
+            <input type={initState.type}
+                   style={{background: initState.className}}
                    maxLength={4}
                    onChange={(event) => dispatch(onValueChange(event.target.value))}
                    value={initState.value}
@@ -22,7 +26,7 @@ const DoorLock = () => {
                     <button key={index} className="button" onClick={() => dispatch(btnClick(every))} type="button">{every}</button>
                 ))
             }
-            <button className="button" type="button">*</button>
+            <button className="button" type="button" onClick={() => dispatch(backSpace())}>*</button>
             <button className="button" type="button" onClick={() => dispatch(btnClick("0"))}>0</button>
             <button className="button" type="submit">E</button>
         </form>
--- src/containers/DoorLock/doorLockSlice.ts
+++ src/containers/DoorLock/doorLockSlice.ts
@@ -3,29 +3,67 @@ import {createSlice} from "@reduxjs/toolkit";
 export interface DoorState {
     value: string;
     type: string;
+    className: string;
 }
 
 const initialState: DoorState = {
     value: "",
-    type: "",
+    type: "password",
+    className: "none",
 };
+
+const password = "2835";
 
 export const doorLockSlice = createSlice({
     name: "doorLock",
     initialState,
     reducers: {
         onValueChange: (state, action) => {
+            if (state.value === "Access Granted" ||  state.value === "Access Denied") {
+                state.value = "";
+                state.type = "password";
+                state.className = "none";
+            }
+            state.className = "none";
+            state.type = "password";
             state.value = action.payload;
         },
 
         btnClick: (state, action) => {
+            if (state.value === "Access Granted" ||  state.value === "Access Denied") {
+                state.value = "";
+                state.type = "password";
+                state.className = "none";
+            }
             if (state.value.length < 4) {
+                state.className = "none";
+                state.type = "password";
                 state.value += action.payload;
             }
-            console.log(state.value);
         },
+
+        backSpace: (state) => {
+            if (state.value === "Access Granted" ||  state.value === "Access Denied") {
+                state.value = "";
+                state.type = "password";
+                state.className = "none";
+            }
+            state.value = state.value.slice(0, state.value.length -1);
+        },
+
+        enter: (state) => {
+            state.type = "text";
+            if (state.value === password) {
+                state.className = "green";
+                state.value = "Access Granted";
+            } else {
+                state.className = "red";
+                state.value = "Access Denied";
+            }
+        }
     }
 });
 
 export const doorLockReducer = doorLockSlice.reducer;
-export const {onValueChange, btnClick,} = doorLockSlice.actions;
+export const {onValueChange, btnClick,
+    backSpace, enter} = doorLockSlice.actions;
